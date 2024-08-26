917e31439115228c2acc3e2a09cca9b37980badc
Jihyun <parkjh2910@gmail.com>
2024-08-26 15:28:04 +0900

    feat - get() 추가

--- src/main/java/landvibe/PI_Server/Controller.java
+++ src/main/java/landvibe/PI_Server/Controller.java
@@ -1,7 +1,17 @@
 package landvibe.PI_Server;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class Controller {
+
+    AtomicInteger num = new AtomicInteger(0);
+
+    @GetMapping
+    public String getCount() {
+        return "현재 숫자는 " + num.get();
+    }
 }
